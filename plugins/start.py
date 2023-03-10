1b5413dcd594e4d0939cfaf8e234e8462696fbe7
hyper-ub <122169553+hyper-ub@users.noreply.github.com>
2023-03-10 17:45:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
-
+from plugins import ratelimiter
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
