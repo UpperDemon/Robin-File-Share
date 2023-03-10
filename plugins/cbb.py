18a9ea53e858b3026947b969ed83e6ac729b70da
Not Real <112511577+UpperDemon@users.noreply.github.com>
2023-03-10 15:10:01 +0530

    Create cbb.py

---
+++ plugins/cbb.py
@@ -0,0 +1,29 @@
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f'''<b>Step 1. Join the channel 
+Step 2. Then tap on link again or come back to bot and click in Try Again. 
+Step 3. Tap on Start and Done ✅
+</b>''',
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
