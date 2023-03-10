3200688812c32f56e93cfeb2a687c091fbfe6ac7
hyper-ub <122169553+hyper-ub@users.noreply.github.com>
2023-03-10 17:32:02 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,5 +1,6 @@
 #(©)Codexbotz
 
+from plugins import ratelimiter
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from bot import Bot
@@ -7,6 +8,7 @@ from config import ADMINS
 from helper_func import encode, get_message_id
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+@ratelimiter
 async def batch(client: Client, message: Message):
     while True:
         try:
