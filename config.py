cc7d202f5450faecbdc27bf437fc42d49dd20221
hyper-ub <122169553+hyper-ub@users.noreply.github.com>
2023-03-13 19:50:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from logging.handlers import RotatingFileHandler
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID"," "))
+APP_ID = os.environ.get("APP_ID","")
 
 #Your API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", " ")
