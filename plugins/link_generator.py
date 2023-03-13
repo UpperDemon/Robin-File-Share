acf82c64602e504226eee8fae5e768066b452f33
hyper-ub <122169553+hyper-ub@users.noreply.github.com>
2023-03-13 08:52:46 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -8,7 +8,7 @@ from config import ADMINS
 from helper_func import encode, get_message_id
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
-@ratelimiter
+#@ratelimiter
 async def batch(client: Client, message: Message):
     while True:
         try:
