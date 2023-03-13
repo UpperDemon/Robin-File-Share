954f06f67ad32ab89f0cab3bfcb965b82c4681c4
Not Real <112511577+UpperDemon@users.noreply.github.com>
2023-03-13 20:27:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import pyromod.listen
 from pyrogram import Client
 from pyrogram.enums import ParseMode
 import sys
+import asyncio
 from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
