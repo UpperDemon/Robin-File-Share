c6d69ac055d02880a50ced38513c5be670f42139
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2023-11-02 13:21:49 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,8 @@ async def start_command(client: Client, message: Message):
                 ]
             ]
         )
-        await message.reply_text(
-            text = START_MSG.format(
+        await message.reply_photo(
+            caption = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,
