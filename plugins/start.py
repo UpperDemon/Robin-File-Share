9373e44e6d7ee7b543ad7bfff3ad8c66751668dc
Sahil Pandey <112511577+UpperDemon@users.noreply.github.com>
2024-06-27 19:04:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -147,7 +147,7 @@ async def not_joined(client: Client, message: Message):
             ),
         reply_markup = InlineKeyboardMarkup(buttons),
         quote = True,
-        disable_web_page_preview = false
+        disable_web_page_preview = False
     )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
