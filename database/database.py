97951f0a1d44dce8d0c3e3df48e9c531d7d1274d
Sahil Pandey <112511577+UpperDemon@users.noreply.github.com>
2024-06-27 17:20:45 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,35 +1,26 @@
-#(©)CodeXBotz
-
-
-
-
-import pymongo, os
+import pymongo
+import os
 from config import DB_URI, DB_NAME
-
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
 
-
 user_data = database['users']
 
-
-
-async def present_user(user_id : int):
+async def present_user(user_id: int):
     found = user_data.find_one({'_id': user_id})
     return bool(found)
 
-async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
+async def add_user(user_id: int, username: str, name: str):
+    user_data.insert_one({'_id': user_id, 'username': username, 'name': name})
     return
 
 async def full_userbase():
     user_docs = user_data.find()
-    user_ids = []
+    users = []
     for doc in user_docs:
-        user_ids.append(doc['_id'])
-        
-    return user_ids
+        users.append({'user_id': doc['_id'], 'username': doc.get('username'), 'name': doc.get('name')})
+    return users
 
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
